10b61000c86988a3fbdd893634c0f2719795398f
Michael Rosabal <mrosabal@circleci.com>
2017-05-05 19:04:37 -0400

    Quick fixes, ebs volumes, and file rename

--- mongodbs.tf
+++ mongodbs.tf
@@ -5,12 +5,12 @@ resource "aws_instance" "ccie_mongodb" {
     ami           = "${lookup(var.base_services_image, var.aws_region)}" # Probably need to change this
     count         = 3
     key_name      = "${var.aws_ssh_key_name}"
-    subnet_id     = "${element(list(var.aws_subnet_id, var.aws_subnet_id_2), count.index)}"
+    subnet_id     = "${element(list(var.aws_subnet_id, var.aws_subnet_id_2,var.aws_subnet_id_3), count.index)}"
     vpc_security_group_ids = ["${aws_security_group.circle_mongo_sg.id}"]
     #iam_instance_profile = "${module.vpc.circleci_instance_profile_name}"
 
     user_data = <<EOF
-!/bin/bash
+#!/bin/bash
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
@@ -18,6 +18,9 @@ sudo apt-get install -y mongodb-org=3.2.3 mongodb-org-server=3.2.3 mongodb-org-s
 # mongo-specific configuration / optimization
 echo 'never' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
 echo 'never' | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
+# Configure Replica Set
+mkdir -p ~/mongosvr/rs-${count.index}
+mongod --dbpath ~/mongosvr/rs-${count.index}
 EOF
     disable_api_termination = false
     lifecycle {
@@ -26,6 +29,26 @@ EOF
     tags {
         Name      = "${var.prefix}_mongodb_${count.index + 1}"
     }
+}
+
+resource "aws_ebs_volume" "ccie_mongodb_ebs" {
+    count = 3
+    type      = "io1"
+    size      = "300"
+    iops      = "100"
+    encrypted = true
+    #availability_zone = "${element(format("aws_instance.ccie_mongodb.%d.availability_zone", count.index),0)}"
+    availability_zone = "${element(aws_instance.ccie_mongodb.*.availability_zone, count.index)}"
+    tags {
+        Name      = "${var.prefix}_mongodb_ebs_${count.index + 1}"
+    }
+}
+
+resource "aws_volume_attachment" "ccie_mongodb_ebs_attach" {
+  count = 3
+  device_name = "/dev/sdi"
+  volume_id   = "${element(aws_ebs_volume.ccie_mongodb_ebs.*.id, count.index)}"
+  instance_id = "${element(aws_instance.ccie_mongodb.*.id, count.index)}"
 }
 
 resource "aws_security_group" "circle_mongo_sg" {  
